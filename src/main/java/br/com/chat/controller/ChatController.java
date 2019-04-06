65742a5c6dac4517780622113c374a0c6a78d8f6
fernando.h.alves <fernando.h.alves@kroton.com.br>
2019-04-06 16:46:45 -0300

    Remove unused method

--- src/main/java/br/com/chat/controller/ChatController.java
+++ src/main/java/br/com/chat/controller/ChatController.java
@@ -3,7 +3,9 @@ package br.com.chat.controller;
 import br.com.chat.model.Chat;
 import br.com.chat.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,11 +15,6 @@ public class ChatController {
 
     @Autowired
     private ChatService chatService;
-
-    @PostMapping
-    public Chat save(@RequestBody Chat cartao) {
-        return chatService.save(cartao);
-    }
 
     @GetMapping
     public List<Chat> getAll() {
